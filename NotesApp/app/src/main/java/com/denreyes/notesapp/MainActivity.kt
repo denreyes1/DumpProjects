72087df7f5a44d8953dccb6801162d4d6c52012e
Densho <denj.rey.c@gmail.com>
2026-03-05 15:19:22 -0500

    Button beside or below TextField labeled Add Note

--- NotesApp/app/src/main/java/com/denreyes/notesapp/MainActivity.kt
+++ NotesApp/app/src/main/java/com/denreyes/notesapp/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.denreyes.notesapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,11 +30,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.denreyes.notesapp.ui.theme.NotesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,9 +77,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentBody(modifier: Modifier = Modifier) {
-    var text by remember { mutableStateOf("") }
-    Column(modifier = modifier) {
-        TextField(value = text, onValueChange = { text = it }, label = { Text("Type note here...") })
+    var string by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    Column(modifier) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            TextField(
+                value = string,
+                onValueChange = { string = it },
+                modifier = Modifier.weight(1f),
+                label = { Text("Type note...") })
+            Spacer(modifier.width(8.dp))
+            Button(onClick = {
+                Toast.makeText(context, "Button Clicked!", Toast.LENGTH_LONG).show()
+            }, modifier = Modifier.wrapContentWidth()) {
+                Text("Add Note")
+            }
+        }
     }
 }
 
